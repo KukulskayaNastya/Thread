3134feac74ab0f06841b2ea47497528081252bcb
KukulskayaNastya <nastena_2705@mail.ru>
2017-04-25 20:57:42 +0400

    Initial commit

---
+++ src/Counter.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class Counter {
+    int t;
+
+    public Counter(int t) {
+        this.t=t;
+    }
+
+    public void start1() throws InterruptedException {
+        MyCode code = new MyCode(t);
+
+        Thread[] threads = new Thread[4];
+        for (int i=0; i<threads.length; i++){
+            threads[i] = new Thread(code);
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+    }
+
+    public class MyCode implements Runnable{
+        int t;
+
+        public MyCode(int t){
+            this.t = t;
+        }
+
+        public void run() {
+            for (int i=1;i<=10000;i++) {
+//               int h=getFree();
+                synchronized (this) {
+                    System.out.println(getFree());
+                }
+            }
+        }
+
+        public int getFree(){
+//            for (int i=0 ;i <=1000; i++) {
+//                double[] s = {1.2412, 24.214124, 24.214125, 26.436436, 1.2412, 24.214124, 24.214125, 26.436436, 1.2412, 24.214124, 24.214125, 26.436436};
+//                Arrays.sort(s);
+//            }
+            return t++;
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Counter t = new Counter(1);
+        t.start1();
+    }
+}
